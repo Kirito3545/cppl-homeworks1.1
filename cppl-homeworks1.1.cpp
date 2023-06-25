ec79fd935a6699322432fcfd7d87500be22468df
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-06-26 01:52:16 +0300

    Initial commit

---
+++ cppl-homeworks1.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    std::vector<int> myVector = { 4, 7, 9, 14, 12};
+
+    auto print = [](const int& n) { std::cout << n << '\t'; };
+
+    std::cout << "before:\t";
+    std::for_each(myVector.cbegin(), myVector.cend(), print);
+    std::cout << '\n';
+
+    std::for_each(myVector.begin(), myVector.end(), [&myVector](int& i)
+        {
+            if (i % 2 != 0)
+            {
+                (i) *= 3;
+            }
+        });
+
+    std::cout << "after:\t";
+    std::for_each(myVector.cbegin(), myVector.cend(), print);
+    std::cout << '\n';
+}
